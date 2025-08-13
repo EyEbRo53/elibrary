0b157c18f048a333147760d8fd55398179d38495
BensonRaro <bensonraro22@gmail.com>
2025-08-13 18:34:59 +0300

    update

--- app/dashboard/[userId]/books/[id]/page.tsx
+++ app/dashboard/[userId]/books/[id]/page.tsx
@@ -3,10 +3,10 @@ import { db } from "@/drizzle";
 
 const NewBooks = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
-  const newId = id === "new" ? null : id;
+  const newId = id === "new" ? "" : id;
 
   const book = await db.query.books.findFirst({
-    where: (book, { eq }) => eq(book.id, newId || ""),
+    where: (book, { eq }) => eq(book.id, newId),
   });
   return (
     <section className="w-full">
